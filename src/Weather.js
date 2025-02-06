a798f20d6b8396554d9110a1488f8c61305caae2
Ruta-B <baumaneruta38@gmail.com>
2025-02-06 21:18:36 +0100

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -11,5 +11,5 @@ export default function Weather(props) {
   let apiKey = "ed55b36e362d8733f7d859247cedeaf2";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello</h2>;
+  return <h2>Hi</h2>;
 }
