b420c5eb3da27360dba99c27f336f3a1c19a2905
Ruta-B <baumaneruta38@gmail.com>
2025-02-07 20:19:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,24 @@ function App() {
         <h1>Weather App</h1>
         <Weather />
       </header>
+      <p>
+        This react app was built by Ruta Baumane and is open-sourced on
+        <a
+          href="https://github.com/Ruta-B/test-react-a"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub{" "}
+        </a>
+        ,{" "}
+        <a
+          href="https://react-test-weather-a.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify.
+        </a>
+      </p>
     </div>
   );
 }
